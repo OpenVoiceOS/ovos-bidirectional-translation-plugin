0f314d7df79ceee008f3887de643c02ac0f91040
miro <jarbasai@mailfence.com>
2024-09-10 21:53:39 +0100

    cgore:automations

--- setup.py
+++ setup.py
@@ -1,13 +1,37 @@
 from setuptools import setup
 from os import path, getenv
-
+import os
 UTTERANCE_ENTRY_POINT = 'ovos-utterance-translation-plugin=ovos_bidirectional_translation_plugin:UtteranceTranslator'
 DIALOG_ENTRY_POINT = 'ovos-dialog-translation-plugin=ovos_bidirectional_translation_plugin:DialogTranslator'
 
+def get_version():
+    """ Find the version of the package"""
+    version = None
+    version_file = os.path.join('ovos_bidirectional_translation_plugin', 'version.py')
+    print(f"ERROR: version file: {version_file}")
+    major, minor, build, alpha = (None, None, None, None)
+    with open(version_file) as f:
+        for line in f:
+            if 'VERSION_MAJOR' in line:
+                major = line.split('=')[1].strip()
+            elif 'VERSION_MINOR' in line:
+                minor = line.split('=')[1].strip()
+            elif 'VERSION_BUILD' in line:
+                build = line.split('=')[1].strip()
+            elif 'VERSION_ALPHA' in line:
+                alpha = line.split('=')[1].strip()
+
+            if ((major and minor and build and alpha) or
+                    '# END_VERSION_BLOCK' in line):
+                break
+    version = f"{major}.{minor}.{build}"
+    if alpha and int(alpha) > 0:
+        version += f"a{alpha}"
+    return version
 
 setup(
     name='ovos-bidirectional-translation-plugin',
-    version="0.0.0a1",
+    version=get_version(),
     description='Bidirectional translation plugin to make OVOS understand any language',
     url='https://github.com/OpenVoiceOS/ovos_bidirectional_translation_plugin',
     author='jarbasAi',
